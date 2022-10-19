426c8502b19e3ffe08adecb2c1bc1474106a4989
Allen Mordukhaev <allenmor93@gmail.com>
2022-10-19 17:59:46 -0400

    logo up

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import logo from '../images/logo.png'
+import './NavBar.css'
+
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div style={{['background-image']:[` url(${logo})`]}} className='navbar-container'>
+
+    </div>
   )
 }
 
